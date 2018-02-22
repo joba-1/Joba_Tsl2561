1707d90fa12423a64bb2ec2dfa0a8d29a935f228
Joba-1 <joban123.psn@gmail.com>
2018-02-22 13:57:06 +0100

    Patch to remove links for arduino lib manager

--- examples/platformio.sh
+++ examples/platformio.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# Create platformio.ini links for all examples, if missing.
+# Create platformio.ini templates for all examples, if missing.
 # Start this script from within the examples directory.
 
 for d in *; do
   if [ -d "$d" -a -f "$d/$d.ino" -a ! -e "$d/platformio.ini" ]
   then
-    ln -sv ../platformio.ini "$d/"
+    cp -av platformio.ini "$d/"
   fi
 done
 
